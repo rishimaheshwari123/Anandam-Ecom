63f812c6dde4abce0467ce78e13275409293c0d9
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-02-03 12:37:43 +0530

    update images

--- client/src/components/Shop/Layout/DashboardHeader.jsx
+++ client/src/components/Shop/Layout/DashboardHeader.jsx
@@ -18,7 +18,7 @@ const DashboardHeader = () => {
       </div>
       <div className="flex items-center">
         <div className="flex items-center mr-4">
-          <Link to="/dashboard/cupouns" className="800px:block hidden">
+          {/* <Link to="/dashboard/cupouns" className="800px:block hidden">
             <AiOutlineGift
               color="#555"
               size={30}
@@ -48,7 +48,7 @@ const DashboardHeader = () => {
               size={30}
               className="mx-5 cursor-pointer"
             />
-          </Link>
+          </Link> */}
           <Link to={`/shop/${seller._id}`}>
             <img
               src={`${backend_url}${seller.avatar}`}
