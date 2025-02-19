cc7fa7a3d4032e4b257afa9653c25502722344f5
Rishi <rishimaheshwari040@gmail.com>
2025-02-19 16:44:38 +0530

    change url

--- client/src/server.js
+++ client/src/server.js
@@ -1,3 +1,5 @@
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
+// export const backend_url = "http://localhost:8000/";
+export const server = "https://anandam-ecom-backend.onrender.com/api/v2";
+export const backend_url = "https://anandam-ecom-socet.onrender.com/";
 
-export const backend_url = "http://localhost:8000/";
