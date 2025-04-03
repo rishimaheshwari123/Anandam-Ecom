9629a87d38892887ae9a7e52b92571f6b621b876
Rishi <rishimaheshwari040@gmail.com>
2025-04-03 13:59:00 +0530

    up

--- client/src/server.js
+++ client/src/server.js
@@ -1,6 +1,6 @@
-export const server = "http://localhost:8002/api/v2";
-export const backend_url = "http://localhost:8002/";
+// export const server = "http://localhost:8002/api/v2";
+// export const backend_url = "http://localhost:8002/";
 // export const backend_url = "https://socket.anandamecommerce.com/";
-// export const server = "https://anandam-ecom-backend.onrender.com/api/v2";
-// export const backend_url = "https://anandam-ecom-backend.onrender.com/";
+export const server = "https://api.anandamecommerce.com/api/v2";
+export const backend_url = "https://api.anandamecommerce.com/";
 
